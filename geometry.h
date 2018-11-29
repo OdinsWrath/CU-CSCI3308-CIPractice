74f7fd4e410c09d01b5f508bf32806b63d300687
OdinsWrath <mtlhead93@gmail.com>
2018-11-28 17:01:20 -0700

    Final Push

--- geometry.h
+++ geometry.h
@@ -27,4 +27,4 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
 
 /* Returns the area of the area of the triangle formed by the
 three argument points */
-double coord_2d_area_trangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c);
+double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c);
